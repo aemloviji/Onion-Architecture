092e7406100765717e719e4ce8821056061e2283
AEMLoviji <AEMLoviji@gmail.com>
2019-03-27 18:53:07 +0300

    Domain Model and Domain Services Layers added

---
+++ OnionArchitecture.Domain.Core/Product.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OnionArchitecture.Domain.Core
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ OnionArchitecture.Domain.Interfaces/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using OnionArchitecture.Domain.Core;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace OnionArchitecture.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetByIdAsync(int id);
+        Task<List<Product>> ListAsync();
+        Task AddAsync(Product product);
+        Task UpdateAsync(Product product);
+    }
+}
